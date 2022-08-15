6d09c85f1fbd94aafe6108f1189ab30713afd62b
whitetiger1002 <nike199000@outlook.com>
2022-08-15 10:32:39 +0800

    Solana project

---
+++ src/components/AuctionCard/hooks/useInstantSaleState.ts
@@ -0,0 +1,45 @@
+import { useWallet } from '@solana/wallet-adapter-react';
+import { AuctionView } from '../../../hooks';
+
+interface ActionButtonContentProps {
+  isInstantSale: boolean;
+  isAlreadyBought: boolean;
+  canClaimItem: boolean;
+  canClaimPurchasedItem: boolean;
+  canEndInstantSale: boolean;
+}
+
+export const useInstantSaleState = (
+  auctionView: AuctionView,
+): ActionButtonContentProps => {
+  const wallet = useWallet();
+
+  const { isInstantSale, auctionManager, auction, myBidRedemption } =
+    auctionView;
+
+  const items = auctionView.items;
+  let isAlreadyBought: boolean = false;
+
+  for (const item of items) {
+    for (const subItem of item) {
+      const bidRedeemed = myBidRedemption?.info.getBidRedeemed(
+        subItem.safetyDeposit.info.order,
+      );
+      isAlreadyBought = bidRedeemed ? bidRedeemed : false;
+      if (isAlreadyBought) break;
+    }
+  }
+  const canClaimPurchasedItem = isAlreadyBought;
+  const isOwner = auctionManager.authority === wallet?.publicKey?.toBase58();
+  const isAuctionEnded = auction.info.endedAt;
+  const canClaimItem = !!(isOwner && isAuctionEnded);
+  const canEndInstantSale = isOwner && !isAuctionEnded;
+
+  return {
+    isInstantSale,
+    isAlreadyBought,
+    canClaimItem,
+    canClaimPurchasedItem,
+    canEndInstantSale,
+  };
+};
